f4cd4a6481a5a4db4906e6d9473c437aeb317eee
M150 <stevovic@gmail.com>
2019-01-01 12:43:24 +0100

    media breakpoints exp

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,12 @@
 import colors from './colors'
 
+const sizes = {
+  wide: 1200,
+  desktop: 992,
+  tablet: 768,
+  phone: 576,
+}
+
 const theme = {
   colors,
   sizes: {
@@ -10,6 +17,18 @@ const theme = {
     small: '35em',
     medium: '50em',
     large: '70em',
+  },
+  minWidth: {
+    wide: `min-width: ${sizes.wide / 16}em`,
+    desktop: `min-width: ${sizes.desktop / 16}em`,
+    tablet: `min-width: ${sizes.tablet / 16}em`,
+    phone: `min-width: ${sizes.phone / 16}em`,
+  },
+  maxWidth: {
+    wide: `max-width: ${sizes.wide / 16}em`,
+    desktop: `max-width: ${sizes.desktop / 16}em`,
+    tablet: `max-width: ${sizes.tablet / 16}em`,
+    phone: `max-width: ${sizes.phone / 16}em`,
   },
 }
 
